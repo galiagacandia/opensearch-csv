e2e4c317f055557fa37fb07ec328ea9b889750b6
Grover Leopoldo Aliaga Candia <galiagacandia@gmail.com>
2022-10-29 12:47:14 -0400

    Commit v1.0.3 opensearch-csv fix.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,6 @@
 import {describe, expect, test} from '@jest/globals';
 import {Client} from "@opensearch-project/opensearch";
-import {load_csv_file} from "./index";
+import {load_csv_file} from "./opensearch-csv";
 import * as fs from "fs";
 
 
@@ -16,24 +16,6 @@ describe('opensearch-csv', function () {
         const auth:string = 'admin:admin';
         const client = new Client({
             node: protocol + '://' + auth + '@' + host + ':' + port,
-        });
-        const indexName = "drop-this-index";
-        const filePath = "./test.csv";
-        const result = await load_csv_file({ client, indexName, filePath });
-        expect(result.count).toBe(3);
-        expect(result.erroredDocuments).toEqual([]);
-    });
-    it('error load_csv_file', async () => {
-        fs.writeFileSync(
-            "./test.csv",
-            `id,name,age\n1,John,25\n2,Jane,24\n3,Jack,23`
-        );
-        const protocol:string = 'http';
-        const host:string = 'localhost';
-        const port:number = 9200;
-        const auth:string = 'admin:admin';
-        const client = new Client({
-            node: protocol + '://' + auth + '@' + host + ':' + port,  //"http://localhost:9200"
         });
         const indexName = "drop-this-index";
         const filePath = "./test.csv";
